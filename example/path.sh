e56c98cba2d5a5fd910c24db73daccbfb81361eb
Doug Salt <doug.salt@hutton.ac.uk>
2022-11-15 13:51:11 +0000

    Dunno

--- example/path.sh
+++ example/path.sh
@@ -2,7 +2,7 @@
 export REQUIRED_NOF_CPUS=4
 #export test=1
 
-export SSREPI_MAX_PROCESSES=100
+export SSREPI_MAX_PROCESSES=256
 export SSREPI_SLURM=1
 #export SSREPI_DBUSER=doug
 export SSREPI_DBUSER=ds42723
